e842b4762f6e2fc700e309ae6bf50a6e46077ee2
Omar Abid <>
2018-11-25 14:05:47 -0500

    deleted Jupyter Notebook

--- mlserver/PredictorDarknet.py
+++ mlserver/PredictorDarknet.py
@@ -11,7 +11,7 @@ from object_detection.utils import label_map_util
 
 
 class DarknetYOLO(threading.Thread):
-    
+
     def __init__(self, image_data,
                          YOLO_DIR,
                          score_thresh=0.5,
@@ -23,16 +23,16 @@ class DarknetYOLO(threading.Thread):
         YOLO_WEIGHTS =  glob.glob(YOLO_DIR + '*.weights')[0]
 
         CLASS_NAMES =  glob.glob(YOLO_DIR + '*.names')[0]
-        
-        PATH_TO_LABELS =  self.createClassNames(YOLO_DIR, CLASS_NAMES)
+
+        self.createClassNames(YOLO_DIR, CLASS_NAMES)
         ENABLE_BY_DEFAULT = False
         self.done = False
         threading.Thread.__init__(self)
-        [self.category_index, self.NUM_CLASSES] = self.get_label_map(PATH_TO_LABELS)
+
         self.pause = False
         self.name = "YOLO Predictor Thread"
-        
-        
+
+
         self.image_data = image_data
         self.net = net = Detector(bytes(YOLO_CFG, encoding="utf-8"), bytes(YOLO_WEIGHTS, encoding="utf-8"), 0,
                bytes(YOLO_DATA, encoding="utf-8"))
@@ -42,8 +42,8 @@ class DarknetYOLO(threading.Thread):
         self.output_data.score_thresh = score_thresh
         self.output_data.category_index = self.category_index
         self.frames_per_ms = fps;
-    
-    
+
+
     def createDataFile(self, YOLO_DIR):
         FILE_DATA = YOLO_DIR + YOLO_DIR.split('/')[-2] + '.data'
         FILE_NAMES = glob.glob(YOLO_DIR + '*.names')[0]
@@ -52,37 +52,20 @@ class DarknetYOLO(threading.Thread):
         f.write('classes= ' + str(NUM_CLASSES) + '\n')
         f.write('names= ' + str(FILE_NAMES) + '\n')
         f.close()
-        
+
     def createClassNames(self,YOLO_DIR, CLASS_NAMES):
         self.__BEGIN_STRING = ''
-        self.CLASS_NAMES = [self.__BEGIN_STRING + str(s) 
+        self.CLASS_NAMES = [self.__BEGIN_STRING + str(s)
                             for s in pd.read_csv(CLASS_NAMES,header=None,names=['LabelName']).LabelName.tolist()]
 
         # Remove all of the odd characters
         for indx,x in enumerate(self.CLASS_NAMES):
             if "'" in x:
                 self.CLASS_NAMES[indx] = x.replace("'","")
-        # Create Label Map
-        create_label_map_from_list(self.CLASS_NAMES,YOLO_DIR)
-        PATH_TO_LABELS = glob.glob(YOLO_DIR + '*.pbtxt')[0]
         return PATH_TO_LABELS
-    
-    # Loading label map
-    def get_label_map(self,PATH_TO_LABELS):
-        '''
-        Returns the LABEL MAP and NUMBER of classes.
-        '''
-        NUM_CLASSES = 50000
-        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                                    use_display_name=True)
-        category_index = label_map_util.create_category_index(categories)
-        num_classes = len(category_index)
-        return [category_index, num_classes]
-    
 
-        
-        
+
+
     def getLabelIndex(self,class_):
         class_ = str(class_.decode("utf-8"))
         # Get the remapped label
@@ -148,22 +131,5 @@ class DarknetYOLO(threading.Thread):
         '''
         self.output_data.image_data.original_image_np = self.image_data.image_np
         self.output_data.image_data.image_np = self.image_data.image_np
-        
+
         return self.output_data.image_data.image_np
-
-        
-        
-def create_label_map_from_list(label_list,location):
-    # Extract the subset labels that we are concerned about
-
-    # Build the Label_map.pbtxt file
-    dst_file = location + '/label_map.pbtxt'
-    f = open(dst_file, 'w+')
-    index = 1
-    for label in label_list:
-        output_str = 'item {\n  id: '+str(index) + '\n  name: \'' + label + '\'\n}\n'
-        f.write(output_str)
-        index += 1
-    f.close()
-    print("Finished creating labelmap. Saved in " + dst_file)
-    return dst_file
--- mlserver/mlserverclient.py
+++ mlserver/mlserverclient.py
@@ -12,13 +12,16 @@ from ZeroMQ import ZeroMQImageInput
 import zmq
 print("Finished Loading Imports")
 
+import os
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
 context = zmq.Context()
 
 thread_image = ZeroMQImageInput(context);
 thread_image.start()
 
 thread_yolo = DarknetYOLO(thread_image.image_data,
-                        YOLO_DIR="/home/omar_abid4_gmail_com/nnModels/yolov3/coco/608/",
+                        YOLO_DIR=ROOT + "/model/",
                           score_thresh=0.5,
                          fps = 0.08)
 thread_yolo.start()
